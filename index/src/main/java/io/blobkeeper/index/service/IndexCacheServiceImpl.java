64ba9324fb3c30b79ae47a18ff5d1d575b9f3634
Denis M. Gabaidulin <gabaden@gmail.com>
2016-05-10 18:51:51 +0300

    * headers are fixed

--- index/src/main/java/io/blobkeeper/index/service/IndexCacheServiceImpl.java
+++ index/src/main/java/io/blobkeeper/index/service/IndexCacheServiceImpl.java
@@ -1,5 +1,24 @@
 package io.blobkeeper.index.service;
 
+/*
+ * Copyright (C) 2016 by Denis M. Gabaydulin
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import io.blobkeeper.index.domain.CacheKey;
@@ -10,11 +29,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
 
-
-/**
- * @author Denis Gabaydulin
- * @since 15/02/2016
- */
 @Singleton
 public class IndexCacheServiceImpl implements IndexCacheService {
     private static final Logger log = LoggerFactory.getLogger(IndexCacheServiceImpl.class);
